8d3a8169afbd6f2ba070bb766f99e8b68fdc165d
Gabbasov98 <74816663+Gabbasov98@users.noreply.github.com>
2021-05-17 15:55:07 +0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,19 +59,51 @@ function slidersWhy() {
 }
 
 
+function openForm() {
+    $(".modal-bg").show();
+    $(".modal-form").slideDown(200);
+}
 
+function closeForm() {
+    $(".modal-bg").hide();
+    $(".modal-form").slideUp(200);
+}
 
 
 $(document).ready(function() {
     slidersDesc();
     slidersWhy();
+    let xRadio = $("input[name='x']:checked").val();
+    let yRadio = $("input[name='y']:checked").val();
+    let zRadio = $("input[name='z']:checked").val();
+    let result = (xRadio * 3000) + (yRadio * 4500) + (zRadio * 35000) - 250000;
+    $("#result").html(result);
 
-    $("#take-result").click(function() {
+    $(".radio-style").click(function() {
         let xRadio = $("input[name='x']:checked").val();
         let yRadio = $("input[name='y']:checked").val();
         let zRadio = $("input[name='z']:checked").val();
         let result = (xRadio * 3000) + (yRadio * 4500) + (zRadio * 35000) - 250000;
         $("#result").html(result);
+    })
+    $(".why input[type='radio']").click(function() {
+        let xRadio = $("input[name='x']:checked").val();
+        let yRadio = $("input[name='y']:checked").val();
+        let zRadio = $("input[name='z']:checked").val();
+        let result = (xRadio * 3000) + (yRadio * 4500) + (zRadio * 35000) - 250000;
+        $("#result").html(result);
+    })
 
+    $(".model__card-btn").click(function() {
+        openForm();
+    })
+    $(".calc__btn").click(function() {
+        openForm();
+    })
+    $(".cities__btn").click(function() {
+        openForm();
+    })
+    $(".modal-form__img").click(function() {
+        closeForm();
     })
 });
